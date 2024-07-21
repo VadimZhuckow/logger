b1caf3bc4b641e59c33b4542daa1fe5730fdbb0f
VadimZhuckow <apex69knight@gmail.com>
2024-07-21 12:20:51 +0300

    add logger

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from loguru import logger
 
 logger.add("debug.log", format="{time} {level} {message}",
-           level="DEBUG")
+           level="DEBUG", rotation="10 MB", compression='zip', serialize=True)
+
+# for _ in range(1000):
+#     logger.debug('debug message (debug)')
+# logger.info('info message (info)')
+# logger.error('error message (error)')
 
 
-logger.debug('debug message (debug)')
-logger.info('info message (info)')
-logger.error('error message (error)')
+def divide(a, b):
+    return a / b
 
+@logger.catch
+def main():
+    divide(1, 0)
+
+
+main()
